17d2f8f9b494c460ad825d392e359983c29d6773
dimohamdy <dimo.hamdy@gmail.com>
2018-03-08 03:14:40 +0200

    add observer
    add observer to "captured" keypath

--- RecordScreenDetect/ViewController.swift
+++ RecordScreenDetect/ViewController.swift
@@ -13,8 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //  Converted to Swift 4 by Swiftify v4.1.6632 - https://objectivec2swift.com/
+        UIScreen.main.addObserver(self, forKeyPath: "captured", options: .new, context: nil)
+
     }
 
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        if (keyPath == "captured") {
+            let isCaptured = UIScreen.main.isCaptured
+            //blablabla
+            print(isCaptured)
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
